a8cc29de0e03f8373ccfba2443aa236134d4ee35
katamzz <145641456+katamzz@users.noreply.github.com>
2023-09-21 00:32:24 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const yesbtn = document.querySelector("#yesbtn");
+
+yesbtn.addEventListener("click",function () {
+    alert("feliz 21 de sept wuuwuw ella sabia q el sabia q alg pasaria q vendria a buscarla con sus flores amarillaaass")
+}); 
+
+const nobtn = document.querySelector("#nobtn");
+
+nobtn.addEventListener("click",function () {
+    alert("Error intentalo otra vez :c bobo hpta")
+}) 
